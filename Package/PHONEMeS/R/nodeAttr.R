c0a80be05b96e6aaabf11efeab128fe9b9c8ab4e
jakob-wirbel <jakob.wirbel@gmail.com>
2017-01-02 10:32:49 +0100

    Fixed bug that introduced a space in the name of one of the node-tables for the cytoscape networks. Additionally fixed the problem that this node-tables had no header, so that it can be imported into cytoscape more easily.

--- Package/PHONEMeS/R/nodeAttr.R
+++ Package/PHONEMeS/R/nodeAttr.R
@@ -1,8 +1,9 @@
 nodeAttr<-function(targets.On, nodesOF, drugsD, dataGMM, optParam, resFolder=getwd()){
 	#These write the nodes attributes file (1 general and 1 for each condition)
-	write.table(cbind(
+	write.table(rbind(c('Name', 'nodesP'),
+	                  cbind(
               c(unlist(targets.On),unlist(nodesOF$On)), 
-              c(rep("D",length(unlist(targets.On))), rep("P", length(unlist(nodesOF$On))))),
+              c(rep("D",length(unlist(targets.On))), rep("P", length(unlist(nodesOF$On)))))),
             file=paste(resFolder,"AllNodes_nodesP_NA_p",resN(optParam),".txt", sep=""),
             sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
 	for(i in 1:length(targets.On)){
@@ -14,5 +15,5 @@ nodeAttr<-function(targets.On, nodesOF, drugsD, dataGMM, optParam, resFolder=get
     }
 	##write the data table
 	dT<-dataTable(nodesP=unlist(nodesOF$On), drugs=drugsD, dataGMM=dataGMM)
-	write.table(dT, row.names=FALSE, col.names=TRUE, quote=FALSE, file=paste(resFolder,"AllNodes_",resN(optParam),"_DA.txt"), sep="\t")
+	write.table(dT, row.names=FALSE, col.names=TRUE, quote=FALSE, file=paste0(resFolder,"AllNodes_",resN(optParam),"_DA.txt"), sep="\t")
 }
