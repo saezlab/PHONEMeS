7a3239dbee18247537b66b8c18fdcf414a596cba
Sophia Müller-Dott <sophia.md@hotmail.de>
2021-11-09 13:55:52 +0100

    removed files from PHONEMeS v1.0.0

--- Package/PHONEMeS/R/buildNw.R
+++
@@ -1,6 +0,0 @@
-buildNw<-function(data.On,targets.On, bg,
-                    nK=c("all","no", "drugs2data", "data")){
-      pkn<-buildPKN(data.On, targets.On, bg,nK=nK) 
-      pknList<-PKNlist(pkn, targets.On, data.On)           
-      return(pknList)   
-}
